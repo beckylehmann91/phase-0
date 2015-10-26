e2630a8c387dd2668f49f3cd8455be1686642a57
beckylehmann91 <beckylehmann91@gmail.com>
2015-10-26 09:47:54 -0400

    Add solution and spec fiels for define hash

---
+++ week-4/count-between/my_solution.rb
@@ -0,0 +1,22 @@
+# Count Between
+
+# I worked on this challenge by myself.
+
+# count_between is a method with three arguments:
+#   1. An array of integers
+#   2. An integer lower bound
+#   3. An integer upper bound
+#
+# It returns the number of integers in the array between the lower and upper bounds,
+# including (potentially) those bounds.
+#
+# If +array+ is empty the method should return 0
+
+# Your Solution Below
+
+def count_between(list_of_integers, lower_bound, upper_bound)
+  if
+  for num in count_between[1..2]
+    return count_between[0].count
+  end
+end
