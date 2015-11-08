c92b412dee508df1094dd6f97b371e2ca5c63983
beckylehmann91 <beckylehmann91@gmail.com>
2015-11-08 12:49:11 -0600

    salkdfj

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -87,7 +87,50 @@ p card.check_card
 
 # Refactored Solution
 
+class CreditCard
 
+  def initialize(card_number)
+    card_string = card_number.to_s
+    card_digits = card_string.split(//)
+    if card_digits.size == 16
+      @card_number = card_digits
+    else
+      raise ArgumentError.new("Card number needs to be 16 digits.")
+    end
+  end
+
+  def double_digits
+    reverse_digits = @card_number.reverse
+    for i in 0..15
+      if i % 2 == 1
+        reverse_digits[i] = 2 * reverse_digits[i].to_i
+      else
+        reverse_digits[i] = reverse_digits[i].to_i
+      end
+    end
+    @reverse = reverse_digits
+  end
+
+  def sum_digits
+    sum = 0
+    @reverse.each { |i|
+      if i <= 9
+        sum += i
+      else
+        sum += (1 + (i % 10))
+      end
+    }
+    return @sum = sum
+  end
+
+  def check_card
+    if @sum.to_i % 10 == 0
+      return true
+    else
+      return false
+    end
+  end
+end
 
 
 
